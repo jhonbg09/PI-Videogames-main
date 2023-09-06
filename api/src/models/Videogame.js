ac7ceed0ccdec2727498fc95ec0d9d23fb7bf7b9
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-09-06 12:50:23 -0500

    Creando modelos de sequelize

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,54 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    plataforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING), // Colocamos en DataTypes.ARRAY para colocar en un arreglo  varias plataformas y colocamos de nuevo DataType.STRING para indicar que vamos a recibir en el arreglo una cadena de caracteres.
+      allowNull: false
+    },
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    releaseDate: {
+      type: DataTypes.DATE
+    },
+
+    rating: {
+      type: DataTypes.FLOAT, //hace referencia a un numero con coma flotante.
+    }
   });
+
+
+  sequelize.define('genre',{
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true 
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  })
 };
+
+
